0f82c3d6042b11bfbff8c0c5c41d9ba739d909fe
Nikolaus Walton <nikawalton2@gmail.com>
2021-03-06 12:39:32 -0800

    Initial Commit

---
+++ WorldNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldNotes
+//
+//  Created by Nikolaus Walton on 3/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
